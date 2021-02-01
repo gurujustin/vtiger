71f0f4ea0253df6afb8c07bdb61c5a3e404dc14e
stepanBokhanov <kkh0102www@outlook.com>
2021-02-01 07:41:59 -0800

    fix

--- config.inc.php
+++ config.inc.php
@@ -79,7 +79,7 @@ $dbconfigoption['ssl'] = false;
 
 $host_name = $dbconfig['db_hostname'];
 
-$site_URL = 'http://dev.jbms-tech.com/';
+$site_URL = 'https://dev.jbms-tech.com/';
 
 // url for customer portal (Example: http://vtiger.com/portal)
 $PORTAL_URL = $site_URL.'/customerportal';
